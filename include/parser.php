133651b1b0b56c531ccb8b28b0fac5b5f0afdefb
Pavel <d39reg@gmail.com>
2019-07-09 11:48:36 +0200

    Update parser.php

--- include/parser.php
+++ include/parser.php
@@ -240,7 +240,7 @@ function createHouse($rooms=[], $images = [])
 		if ($month2 <= 9) $month2 = '0'.$month2;
 		$d1 = (int)(''.$a['#attr']['year'].$month1);
 		$d2 = (int)(''.$b['#attr']['year'].$month2);
-		return ($d1<$d2);
+		return ($a['#attr']['year']<$b['#attr']['year']);
 	});
 	
 	$offers = [];
