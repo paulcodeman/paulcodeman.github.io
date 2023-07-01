e3c52de5ed7dcc8b556e0afa2a68026091b286cf
Pavel <paulcodeman@gmail.com>
2023-07-01 20:07:10 +0200

    Update console.js

--- console/console.js
+++ console/console.js
@@ -1,289 +1,203 @@
-function console(object)
-{
-  // default variable init
-  var globalThis = this; // pointer on the object
-  var arrayPosition = [];
-  var nbsp = String.fromCharCode(160);
-  var backPosition = null;
-  var currentPosition = 0;
-  var blockConsole = false;
-  var positionAutoScroll = 9999;
-  var focusBlock = true;
-  var tempFunctionDocument = document.onkeydown;
-  var tempBlink = true;
+function Console(element) {
+  // Добавляем классы к элементу
+  element.classList.add('console');
+  element.classList.add('console-focused');
 
-  var nullSymbol = document.createElement('SPAN');
-  object.setAttribute('tabindex','-1');
-  object.appendChild(nullSymbol);
-  object.oncontextmenu = function(){return false;}
-  backPosition = nullSymbol;
-  // ---
-  object.onfocus = function()
-  {
-    //tempFunctionDocument = document.onkeydown;
-    focusBlock = false;
-    globalThis.blink = tempBlink;
-    //document.onkeydown = function(){ return false; }
-  }
-  object.onblur = function()
-  {
-    focusBlock = true;
-    tempBlink = globalThis.blink;
-    globalThis.blink = false;
-    //document.onkeydown = tempFunctionDocument;
-  }
-  // functions for addition symbols in console
-  function insertAfter(elem, refElem) 
-  {
-    return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
-  }
-  function sym(s)
-  {
-    if(s.length > 1) return false;
-    if(s == ' ') return '&nbsp;';
-    if(s == "\t") return '&nbsp;&nbsp;&nbsp;&nbsp;';
-    return s;
-  }
-  // ---
+  // Переменные
+  let symbols = [];
+  let currentPosition = 0;
+  let isBlinking = true;
+  let isBlocked = false;
+  let autoScroll = true;
 
-  // prototypes for constructor
-  this.blink = false;
-  this.getText = function() // get full text console
-  {
-    return object.innerText;
+  // Добавляем нулевой символ
+  const nullSymbol = createSymbol('symbol');
+  element.appendChild(nullSymbol);
+
+  // Функция для создания символа
+  function createSymbol(className) {
+    const symbol = document.createElement('span');
+    symbol.className = className;
+    return symbol;
   }
-  this.fillcolor = 'none';
-  this.color = '#FFF';
-  this.keydown = null;
-  this.keyup = null;
-  this.left = function()
-  {
-    if(currentPosition)
-    {
-      arrayPosition[--currentPosition].className = "symbol";
-      if(!currentPosition) 
-      {
-        backPosition = arrayPosition[0];
-        backPosition.className = "symbol left";
+
+  // Функция для вставки символа после другого символа
+  function insertAfter(newSymbol, refSymbol) {
+    refSymbol.parentNode.insertBefore(newSymbol, refSymbol.nextSibling);
+  }
+
+  // Функция для обновления положения курсора
+  function updateCursorPosition() {
+    symbols.forEach((symbol, index) => {
+      symbol.classList.remove('symbol-active');
+      if (index === currentPosition) {
+        symbol.classList.add('symbol-active');
       }
-      else
-      {
-        backPosition = arrayPosition[currentPosition - 1];
-        backPosition.className = "symbol right";
-      }
+    });
+  }
+
+  // Функция для обновления прокрутки
+  function updateScroll() {
+    if (autoScroll) {
+      element.scrollTop = element.scrollHeight;
     }
   }
-  this.right = function()
-  {
-    if(currentPosition < arrayPosition.length)
-    {
-      if(currentPosition) arrayPosition[currentPosition - 1].className = "symbol";
-      backPosition = arrayPosition[currentPosition++];
-      backPosition.className = "symbol right";
+
+  // Обработчик события keypress
+  function handleKeyPress(event) {
+    if (isBlocked) {
+      return;
     }
+
+    const symbol = createSymbol('symbol-right');
+    symbol.textContent = event.key;
+
+    insertAfter(symbol, symbols[currentPosition - 1] || nullSymbol);
+    symbols.splice(currentPosition, 0, symbol);
+    currentPosition++;
+
+    updateScroll();
   }
-  this.input = null; // event input
-  this.getch = null; // event getch
-  this.delay = 500;
-  this.exit = function()
-  {
-    blockConsole = true;
+
+  // Обработчик события keydown
+  function handleKeyDown(event) {
+    if (isBlocked) {
+      return;
+    }
+
+    switch (event.keyCode) {
+      case 8: // Backspace
+        if (currentPosition > 0) {
+          const symbolToRemove = symbols[currentPosition - 1];
+          element.removeChild(symbolToRemove);
+          symbols.splice(currentPosition - 1, 1);
+          currentPosition--;
+        }
+        break;
+      case 37: // Left arrow
+        if (currentPosition > 0) {
+          currentPosition--;
+        }
+        break;
+      case 39: // Right arrow
+        if (currentPosition < symbols.length) {
+          currentPosition++;
+        }
+        break;
+    }
+
+    updateCursorPosition();
   }
-  this.clear = function()
-  {
-    object.innerHTML = '';
-    arrayPosition = [];
-    backPosition = null;
+
+  // Обработчик события focus
+  function handleFocus() {
+    isBlinking = true;
+    element.classList.add('console-focused');
+  }
+
+  // Обработчик события blur
+  function handleBlur() {
+    isBlinking = false;
+    element.classList.remove('console-focused');
+  }
+
+  // Инициализация консоли
+  function init() {
+    element.setAttribute('tabindex', '-1');
+    element.addEventListener('keypress', handleKeyPress);
+    element.addEventListener('keydown', handleKeyDown);
+    element.addEventListener('focus', handleFocus);
+    element.addEventListener('blur', handleBlur);
+
+    setInterval(function () {
+      if (isBlinking) {
+        symbols[currentPosition]?.classList.toggle('symbol-blink');
+      }
+    }, 500);
+
+    element.focus();
+  }
+
+  // Очистка консоли
+  this.clear = function () {
+    symbols.forEach((symbol) => {
+      element.removeChild(symbol);
+    });
+
+    symbols = [];
     currentPosition = 0;
-    blockConsole = false;
 
-    nullSymbol = document.createElement('SPAN');
-    nullSymbol.className = "symbol right";
-    object.appendChild(nullSymbol);
+    const nullSymbol = createSymbol('symbol-left');
+    element.appendChild(nullSymbol);
+  };
 
-    backPosition = nullSymbol;
-  }
-  this.print = function(text, color, bgcolor, bold) // print text console
-  {
-    if(blockConsole) return false;
-    for(var i = 0; i < text.length; i++)
-    {
-
-      var s = text.charAt(i);
-      if(s == "\r" || s == "\n") 
-      {
-        var o = document.createElement('BR');
-        object.appendChild(o);
-      }
-      else 
-      {
-        var o = document.createElement('SPAN');
-        o.className = "symbol";
-        o.onclick = '';
-        if(bold) o.style.fontWeight = 'bold';
-        if(color != undefined) o.style.color = color;
-        else o.style.color = globalThis.color;
-        if(bgcolor != undefined) o.style.backgroundColor = bgcolor;
-        else o.style.backgroundColor = globalThis.fillcolor;
-        o.innerHTML = sym(text.charAt(i));
-        object.appendChild(o);
-      }
-      object.scrollTop = positionAutoScroll;
+  // Вывод текста в консоль
+  this.print = function (text, color, bgcolor, bold) {
+    if (isBlocked) {
+      return false;
     }
-    object.removeChild(nullSymbol);
-    nullSymbol = document.createElement('SPAN');
-    nullSymbol.className = "symbol";
-    object.appendChild(nullSymbol);
-    backPosition.className = "symbol";
-    backPosition = nullSymbol;
-    arrayPosition = [];
-    currentPosition = 0;
+
+    const lines = text.split('\n');
+
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+
+      if (line) {
+        const lineElement = document.createElement('span');
+        lineElement.className = 'symbol-line';
+
+        if (bold) {
+          lineElement.style.fontWeight = 'bold';
+        }
+        if (color !== undefined) {
+          lineElement.style.color = color;
+        }
+        if (bgcolor !== undefined) {
+          lineElement.style.backgroundColor = bgcolor;
+        }
+
+        for (let j = 0; j < line.length; j++) {
+          const char = line.charAt(j);
+          const symbol = createSymbol('symbol');
+          symbol.textContent = char;
+          lineElement.appendChild(symbol);
+          symbols.push(symbol);
+        }
+
+        element.appendChild(lineElement);
+      }
+
+      if (i < lines.length - 1) {
+        element.appendChild(document.createElement('br'));
+      }
+    }
+
+    updateScroll();
+
     return true;
-  }
-  // ---
-  
-  // handlers console
-  window.addEventListener("keypress",function(event)
-  {
-    if(blockConsole || focusBlock) return false;
-    var s = sym(event.key);
-    if(!s) return;
-    var o = document.createElement("SPAN");
-    o.setAttribute('onclick', '');
-    o.className = "symbol right";
-    o.innerHTML = s;
-    o.style.color = globalThis.color;
-    o.style.backgroundColor = globalThis.fillcolor;
-    
-    if(backPosition != null) backPosition.className = "symbol";
+  };
 
-    if(currentPosition) insertAfter(o, arrayPosition[currentPosition - 1]);
-    else object.insertBefore(o, backPosition);
+  // Установка блокировки консоли
+  this.block = function () {
+    isBlocked = true;
+    element.classList.add('console-blocked');
+  };
 
-    backPosition = o;
-    arrayPosition.splice(currentPosition++,0,o);
-    object.scrollTop = positionAutoScroll;
-  });
+  // Снятие блокировки консоли
+  this.unblock = function () {
+    isBlocked = false;
+    element.classList.remove('console-blocked');
+  };
 
-  window.addEventListener("keyup",function(event)
-  {
-    if(blockConsole || focusBlock) return false;
-    if(event.ctrlKey)
-    {
-      if(globalThis.keyup != null) return globalThis.keyup(event.keyCode);
-      return;
-    }
-    switch(event.keyCode)
-    {
-      case 17: // cntr
-        
-      break;
-    }
-  });
-  window.addEventListener("keydown",function(event)
-  {
-    if(blockConsole || focusBlock) return false;
-    if(event.ctrlKey)
-    {
-      if(globalThis.keydown != null) return globalThis.keydown(event.keyCode);
-      return;
-    }
-    switch(event.keyCode)
-    {
-      case 8: // delete
-        if(currentPosition > 1)
-        {
-          object.removeChild(arrayPosition[--currentPosition]);
-          backPosition = arrayPosition[currentPosition - 1];
-          arrayPosition.splice(currentPosition, 1);
-          arrayPosition[currentPosition-1].className = "symbol right";
-        }
-        else if(currentPosition == 1 && arrayPosition.length == 1) // right
-        {
-          object.removeChild(arrayPosition[0]);
-          nullSymbol.className = "symbol left";
-          arrayPosition = [];
-          currentPosition = 0;
-          backPosition = nullSymbol;
-        }
-        else if(currentPosition == 1) // left
-        {
-          object.removeChild(arrayPosition[0]);
-          arrayPosition.splice(0,1);
-          arrayPosition[0].className = "symbol left";
-          currentPosition = 0;
-          backPosition = arrayPosition[0];
-        }
-      break;
-      case 9: // tab
-        if(blockConsole) return false;
-        /*var o = document.createElement("SPAN");
-        o.className = "symbol right";
-        o.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;";
-        o.style.color = globalThis.color;
-        o.style.backgroundColor = globalThis.fillcolor;
+  // Включение автопрокрутки
+  this.enableAutoScroll = function () {
+    autoScroll = true;
+  };
 
-        if(backPosition != null) backPosition.className = "symbol";
+  // Отключение автопрокрутки
+  this.disableAutoScroll = function () {
+    autoScroll = false;
+  };
 
-        if(currentPosition) insertAfter(o, arrayPosition[currentPosition - 1]);
-        else object.insertBefore(o, backPosition);
-
-        backPosition = o;
-        arrayPosition.splice(currentPosition++,0,o);
-        object.scrollTop = positionAutoScroll;*/
-        event.preventDefault();
-        return false;
-      break;
-      case 13: // enter
-        backPosition.className = "symbol";
-        var o = document.createElement("BR");
-        object.appendChild(o);
-        object.removeChild(nullSymbol);
-        nullSymbol = document.createElement('SPAN');
-        nullSymbol.className = "symbol left";
-        object.appendChild(nullSymbol);
-
-        if(globalThis.input != null)
-        {
-          var text = '', s;
-          for(key in arrayPosition)
-          {
-            var symbolNode = arrayPosition[key];
-            s = symbolNode.innerText;
-            if(s == nbsp) text += ' ';
-            else text += s;
-          }
-          globalThis.input(text);
-        }
-
-        arrayPosition = [];
-        currentPosition = 0;
-        backPosition = nullSymbol;
-        object.scrollTop = positionAutoScroll;
-      break;
-      case 17: // cntr
-        
-      break;
-      case 37: // left
-        globalThis.left();
-      break;
-      case 39: // right
-        globalThis.right();
-      break;
-    }
-    return false;
-  });
-  var countTurn = true;
-  setInterval(function()
-  {
-    if(backPosition == null) return true;
-    countTurn = !countTurn;
-    if(countTurn || !globalThis.blink) 
-    {
-      if(currentPosition) backPosition.className = "symbol right";
-      else backPosition.className = "symbol left";
-    }
-    else backPosition.className = "symbol";
-  },this.delay);
-  object.focus();
+  init();
 }
