8954354a8a46a91b4067731458d1f139c536d993
Pavel <paulcodeman@gmail.com>
2023-07-01 20:14:51 +0200

    Update example.js

--- console/example.js
+++ console/example.js
@@ -1,34 +1,28 @@
-window.onload = function() // wait load page
-{
+window.onload = function() {
   var c = new Console(document.getElementById("console"));
-  var apps = function(text)
-  {
-    if(text == '') { c.print("Введите команду: "); return; }
-    else if(text == 'help')
-    {
+  
+  function apps(text) {
+    if (text === '') {
+      c.print("Введите команду: ");
+      return;
+    } else if (text === 'help') {
       c.print("\rhelp > Список функций:\r");
       c.print("alert - функция вызывает окошко alert\r");
       c.print("clear - очистка экрана\r");
       c.print("exit - функция для завершения\r\r");
-    }
-    else if(text == 'alert')
-    {
+    } else if (text === 'alert') {
       alert("Работает!");
-    }
-    else if(text == 'exit')
-    {
+    } else if (text === 'exit') {
       c.exit();
-    }
-    else if(text == 'clear')
-    {
+    } else if (text === 'clear') {
       c.clear();
+    } else {
+      c.print("Такой команды не существует, для помощи введите help\r");
     }
-    else c.print("Такой комманды не существует, для помощи введите help\r");
-
+    
     apps('');
-
-    //c.print("\r");
   }
+  
   c.input = apps;
-  apps('help'); // run default command line `default`
+  apps('help'); // выполнение команды по умолчанию `help`
 }
